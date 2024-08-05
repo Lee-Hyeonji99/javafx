396242e59d179ba1017c06e7deeb6ded2d935b34
DESKTOP-N7VLCOU\ict-25 <padul1210@gmail.com>
2024-08-05 11:34:32 +0900

    추가함

---
+++ src/main/java/com/itgroup/application/Layout01.java
@@ -0,0 +1,13 @@
+package com.itgroup.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Layout01 extends Application {
+    // 추가된 내용
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
