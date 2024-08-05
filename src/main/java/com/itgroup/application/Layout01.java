93183df2c8d263cf4b71df450a1a5bdfa90682e7
DESKTOP-N7VLCOU\ict-25 <padul1210@gmail.com>
2024-08-05 12:02:18 +0900

    ㅇㅇ

--- src/main/java/com/itgroup/application/Layout01.java
+++ src/main/java/com/itgroup/application/Layout01.java
@@ -5,6 +5,7 @@ import javafx.stage.Stage;
 
 public class Layout01 extends Application {
     // 추가된 내용
+    // 영희가 push 하려고 시도함
 
     @Override
     public void start(Stage stage) throws Exception {
